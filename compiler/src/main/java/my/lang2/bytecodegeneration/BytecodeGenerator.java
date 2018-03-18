96b0935645791a58755f343022abaad02d6925c5
alex <paramonovalexv@gmail.com>
2018-03-18 13:40:01 +0300

    compiler test

--- compiler/src/main/java/my/lang2/bytecodegeneration/BytecodeGenerator.java
+++ compiler/src/main/java/my/lang2/bytecodegeneration/BytecodeGenerator.java
@@ -5,6 +5,7 @@ import my.lang2.bytecodegeneration.instructions.VariableDeclaration;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
 
 import java.util.Queue;
 
@@ -17,6 +18,8 @@ public class BytecodeGenerator implements Opcodes {
                //version ,      acess,       name, signature, base class, interfaes
         cw.visit(52, ACC_PUBLIC + ACC_SUPER, name, null, "java/lang/Object", null);
         {
+            createDefaultConstructor(cw);
+
             //declare static void main
             mv = cw.visitMethod(ACC_PUBLIC + ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
             final long localVariablesCount = instructionQueue.stream()
@@ -36,5 +39,15 @@ public class BytecodeGenerator implements Opcodes {
         cw.visitEnd();
 
         return cw.toByteArray();
+    }
+
+    private void createDefaultConstructor(ClassWriter cw) {
+        MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null);
+        mv.visitCode();
+        mv.visitVarInsn(ALOAD, 0);
+        mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
+        mv.visitInsn(RETURN);
+        mv.visitMaxs(1,1);
+        mv.visitEnd();
     }
 }
--- compiler/src/test/java/my/lang2/CompilerTest.java
+++ compiler/src/test/java/my/lang2/CompilerTest.java
@@ -1,12 +1,18 @@
 package my.lang2;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.File;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
-import static org.junit.Assert.*;
 
 /**
  * Created on 16.03.2018.
@@ -14,15 +20,52 @@ import static org.junit.Assert.*;
 public class CompilerTest {
     @Test
     public void main() throws Exception {
+        final String classPath = "src/test/resources/test.class";
+        delete(classPath);
 
         final Path path = Paths.get("src/test/resources/test.l2");
 
 
-      Compiler compiler = new Compiler();
-       // String script = "var numVar=1\n   print numVar \n";*/
+       Compiler compiler = new Compiler();
+
         compiler.compile(new String[]{path.toAbsolutePath().toString()});
 
+        final Class<?> testClass = load(classPath, "test");
+        final Method mainMethod = testClass.getMethod("main", String[].class);
+        String[] params = null;
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        mainMethod.invoke(null, (Object)params);
+
+
+        Assert.assertEquals("1\r\n" +
+                "\"test\"\r\n",  outContent.toString());
+        //System.setOut(System.out);
+
+        //final Object instance = testClass.newInstance();
+
+        //System.out.println("instance = " + instance);
 
     }
+
+    private Class<?> load(String path, String className) throws MalformedURLException, ClassNotFoundException {
+        final Path pathClss = Paths.get(path);
+        if (Files.exists(pathClss)){
+            final URL url = pathClss.toUri().toURL();
+            ClassLoader cl = new URLClassLoader(new URL[]{url});
+            return cl.loadClass(className);
+        }
+        throw new IllegalArgumentException(String.format("'%s'  not found",path));
+    }
+
+    private void delete(String path) throws IOException {
+        final Path pathClss = Paths.get(path);
+        if (Files.exists(pathClss)){
+            Files.delete(pathClss);
+        }
+    }
+
 
 }
